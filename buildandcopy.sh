0b7c3baaf0a3556ea7b113f7c6c7e6f78fa6c7ac
Marcel Huber <marcelhuberfoo@gmail.com>
2014-04-11 23:33:18 +0200

    build script added

---
+++ buildandcopy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+echo "Start updating from git (y*|n)?"
+read v
+if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
+for n in spl-kmod-dkms spl-utilities zfs-kmod-dkms zfs-utilities; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done
+
+echo "Start building (y*|n)?"
+read v
+if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
+for n in spl-kmod-dkms spl-utilities zfs-kmod-dkms zfs-utilities; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done
+
+repodir=/home/own-repo
+
+echo "Install to $repodir (y*|n)?"
+read v
+if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
+for pkgdir in `find . -name '*.tar.xz' -printf "%h\n" | sort | uniq`; do cpfn=`ls -tc1 $pkgdir/*.tar.xz | head -1`; echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp $cpfn $repodir && repo-add -f -d -n ${repodir}/ownrepo.db.tar.gz $repodir/`basename $cpfn`"; fi; done
+
