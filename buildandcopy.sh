cb817cf34f87d7ae8a2f6f3d1c9d78f153ee3c27
Marcel Huber <marcelhuberfoo@gmail.com>
2014-05-08 15:59:31 +0200

    update of spl and build script

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 thedirs="spl-kmod-dkms spl-utilities zfs-kmod-dkms zfs-utilities"
-set -e
-echo "Start updating from git (y*|n)?"
-read v
-if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
-for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done
-
-echo "Start building (y*|n)?"
-read v
-if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
-for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done
-
 repodir=/home/own-repo
 
-echo "Install to $repodir (y*|n)?"
-read v
-if [ "$v" = "n" -o "$v" = "N" ]; then false; fi
-for pkgdir in `find . -name '*.tar.xz' -printf "%h\n" | sort | uniq`; do cpfn=`ls -tc1 $pkgdir/*.tar.xz | head -1`; echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp $cpfn $repodir && repo-add -f -d -n ${repodir}/ownrepo.db.tar.gz $repodir/`basename $cpfn`"; fi; done
+thingsToDo=(
+  'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done'
+  'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done'
+  'for pkgdir in $(find $thedirs -name ''*.tar.xz'' -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp $cpfn $repodir && repo-add -f -d -n ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn)"; fi; done'
+)
+
+set -e
+
+for n in "${thingsToDo[@]}"; do
+  echo "Continue with [$n] (y*|n)?"
+  read v
+  if [ "$v" = "n" -o "$v" = "N" ]; then
+    echo bla;
+  else
+    eval "$n";
+  fi
+done
 
