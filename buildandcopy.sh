9461ed72fdf50f095cc415fb6bf708d10ca1eb87
Marcel Huber <m1huber@hsr.ch>
2014-05-15 09:29:41 +0200

    only copying repo files if repo directory exists

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -7,7 +7,7 @@ thingsToDo=(
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done'
   'git diff --quiet --no-ext-diff --exit-code || git diff -b -w && git add -p && git commit'
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done'
-  'for pkgdir in $(find $thedirs -name ''*.tar.xz'' -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp --interactive --preserve --update $cpfn $repodir && repo-add --files --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn)"; fi; done'
+  'test -d "$repodir" && for pkgdir in $(find $thedirs -name ''*.tar.xz'' -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp --interactive --preserve --update $cpfn $repodir && repo-add --files --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn)"; fi; done'
 )
 
 set -e
