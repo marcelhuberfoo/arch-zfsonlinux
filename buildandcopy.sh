f248f2590941026af9951b85dae1863fadbf2533
Marcel Huber <m1huber@hsr.ch>
2014-05-15 09:11:16 +0200

    added step to view/add/commit new changes

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -5,6 +5,7 @@ repodir=/home/own-repo
 
 thingsToDo=(
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done'
+  'git diff --quiet --no-ext-diff --exit-code || git diff -b -w && git add -p && git commit'
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done'
   'for pkgdir in $(find $thedirs -name ''*.tar.xz'' -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp $cpfn $repodir && repo-add -f -d -n ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn)"; fi; done'
 )
@@ -15,7 +16,7 @@ for n in "${thingsToDo[@]}"; do
   echo "Continue with [$n] (y*|n)?"
   read v
   if [ "$v" = "n" -o "$v" = "N" ]; then
-    echo bla;
+    echo "..command skipped..";
   else
     eval "$n";
   fi
