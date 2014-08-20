3ba0239b95e3c05aaa6ba6ee5d1bad8101f4ae63
Marcel Huber <marcelhuberfoo@gmail.com>
2014-08-20 15:44:42 +0200

    corrected update of repo db and files

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -7,7 +7,7 @@ thingsToDo=(
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps --nobuild ); done'
   'git diff --quiet --no-ext-diff --exit-code && echo clean || ( git diff -b -w && git add -p && git commit ) || true'
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --noprogressbar --nodeps ); done'
-  'test -n "$repodir" -a -d "$repodir" && for n in $thedirs; do for pkgdir in $(find $n -name "*.tar.xz" -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp --interactive --preserve --update $cpfn $repodir && repo-add --files --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn)"; fi; done; done'
+  'test -n "$repodir" -a -d "$repodir" && for n in $thedirs; do for pkgdir in $(find $n -name "*.tar.xz" -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "copy [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sudo sh -c "cp --interactive --preserve --update $cpfn $repodir && ( repo-add --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn); repo-add --files --new --quiet ${repodir}/ownrepo.files.tar.gz $repodir/$(basename $cpfn))"; fi; done; done'
 )
 
 set -e
