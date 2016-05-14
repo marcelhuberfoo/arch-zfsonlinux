af1cb90a8b09cb2c62d6c00de1dc4d3214a79393
Marcel Huber <marcelhuberfoo@gmail.com>
2016-05-14 23:45:01 +0200

    optimized build processx

--- buildandcopy.sh
+++ buildandcopy.sh
@@ -7,10 +7,9 @@ category=system
 thingsToDo=(
   'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --nodeps --nobuild ); done'
   'git diff --quiet --no-ext-diff --exit-code && echo clean || ( git diff -b -w && git add -p && git commit ) || true'
-  'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --nodeps --clean ); done'
-  'mkaurball -ef'
-  'upfile=$(ls -tc1 *.src.tar.gz | head -1); test -n "$upfile" && ( echo uploading $upfile; burp --user=marcelhuber --category=$category $upfile)'
+  'for n in $thedirs; do ( cd $n && updpkgsums ;  makepkg --force --nodeps --cleanbuild ); done'
   'test -n "$repodir" -a -d "$repodir" && for n in $thedirs; do for pkgdir in $(find $n -name "*.tar.xz" -printf "%h\n" | sort | uniq); do cpfn=$(ls -tc1 $pkgdir/*.tar.xz | head -1); echo "move [$cpfn]? (y*|n)"; read v; if [ ! "$v" = "n" -a ! "$v" = "N" ]; then sh -c "mv --interactive --update $cpfn $repodir && ( repo-add --new --quiet ${repodir}/ownrepo.db.tar.gz $repodir/$(basename $cpfn); )"; fi; done; done'
+  'for n in $thedirs; do ( cd $n && makepkg --nodeps --noextract --force --nobuild --noarchive --nocheck --noprepare --skipinteg --clean ); done'
 )
 
 set -e
